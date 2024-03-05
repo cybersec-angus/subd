28b4695cc0d4230e12a30a1b018c7de9ffe59d41
cybersec-angus <129838933+cybersec-angus@users.noreply.github.com>
2024-03-05 10:10:40 +0000

    Delete .history directory

--- .history/dictionaries/mail_services_20240305100701.py
+++
@@ -1,5 +0,0 @@
-MAIL_SERVICES = {
-    "Protonmail": "*.protonmail.ch",
-    "Google Workspace": "*.google.com",
-    "Zoho": "mx.zoho.eu."
-}
--- .history/dictionaries/mail_services_20240305100955.py
+++
@@ -1,6 +0,0 @@
-MAIL_SERVICES = {
-    "Protonmail": "*.protonmail.ch.",
-    "Google Workspace": "*.google.com.",
-    "Zoho": "*.zoho.eu.",
-    "Simplelogin": "*.simplelogin.co.",
-}
