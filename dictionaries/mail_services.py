cf298dcfb736707028ba3d791af001098007dd97
cybersec-angus <129838933+cybersec-angus@users.noreply.github.com>
2024-03-06 07:04:44 +0000

    Update mail_services.py

--- dictionaries/mail_services.py
+++ dictionaries/mail_services.py
@@ -1,6 +1,19 @@
 MAIL_SERVICES = {
-    "Protonmail": "*.protonmail.ch.",
-    "Google Workspace": "*.google.com.",
+    "Google": "*.google.com.",
+    "Office365": "*.office365.com.",
+    "Office365": "*.mail.protection.outlook.com.",
+    "Proofpoint": "*.pphosted.com.",
     "Zoho": "*.zoho.eu.",
     "Simplelogin": "*.simplelogin.co.",
+    "Fastmail": "*.fastmail.com.",
+    "Tuta": "*.tuta.com.",
+    "Tuta": "*.tutanota.com.",
+    "Mailfence": "*.mailfence.com.",
+    "Posteo": "*.posteo.de.",
+    "Mailbox": "*.mailbox.org.",
+    "Runbox": "*.runbox.com.",
+    "Kolabnow": "*.kolabnow.com.",
+    "Mailbox.org": "*.mailbox.org.",
+    "Protonmail": "*.protonmail.ch.",
+
 }
